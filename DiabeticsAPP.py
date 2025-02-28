bd942368ca99a0e7b5ff924a21ff3dacb088e611
Ajeeba <ajeebahaneefa@gmail.com>
2025-02-28 12:03:58 +0530

    Update DiabeticsAPP.py

--- DiabeticsAPP.py
+++ DiabeticsAPP.py
@@ -16,7 +16,7 @@ st.markdown("""
            
         }
         .main {
-            background-color: white;
+            # background-color: white;
             background-image:url("https://www.shutterstock.com/image-photo/diabetes-concept-blood-sugar-meter-600nw-2464647841.jpg");
             background-size: cover;
             background-blend-mode: overlay;
